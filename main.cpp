a7a8584920e9790316d8fdaa3526b685c1c67dc7
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-02-24 10:52:44 +0200

    fixes

--- main.cpp
+++ main.cpp
@@ -8,33 +8,70 @@
 void runDinaminiaiMasyvai(); 
 void WithRandomNumbers();
 void GenerateRandomNumbersAndNames();
+void WithVectors();
+void WithRandomNumbersArrays(int);
 
 
 int main() {
-    int choice;
+    int choice1;
+    int choice2;
     cout << "Pasirinkite programos veikimo buda: " << endl;
-    cout << "1. Viska rasyti ranka" << endl;
-    cout << "2. Generuoti pazymius" << endl;
-    cout << "3. Generuoti studentus ir pazymius" << endl;
-    cout << "4. Baigti darba" << endl;
-    cin >> choice;
+    cout << "1 - Su vektoriais" << endl;
+    cout << "2 - Su C masyvais" << endl;
+    cin >> choice1;
 
-    switch (choice) {
-        case 1:         
-            runDinaminiaiMasyvai();
-            break;
-        case 2:
-             WithRandomNumbers();
-            break;
-        case 3: 
-            GenerateRandomNumbersAndNames();
-        case 4:
-        cout << "Darbas baigiamas" << endl;
+    switch (choice1) {
+case 1: 
+cout << "1. Viska rasyti ranka" << endl;
+cout << "2. Generuoti pazymius" << endl;
+cout << "3. Generuoti studentus ir pazymius" << endl;
+cout << "4. Baigti darba" << endl;
+cin >> choice2;
 
-        break;
-        default:
-            cout << "Invalid choice!" << endl;
+    switch (choice2) {
+                case 1:         
+                    WithVectors();
+                    break;
+                case 2:
+                    WithRandomNumbers();
+                    break;
+                case 3: 
+                    GenerateRandomNumbersAndNames();
+                    break;
+                case 4:
+                cout << "Darbas baigiamas" << endl;
+
+                break;
+                default:
+                    cout << "Blogas Pasirinkimas" << endl;
+            }
+case 2:
+cout << "1. Viska rasyti ranka" << endl;
+cout << "2. Generuoti pazymius" << endl;
+cout << "3. Generuoti studentus ir pazymius" << endl;
+cout << "4. Baigti darba" << endl;
+cin >> choice2;
+
+    switch (choice2) {
+                case 1:         
+                    runDinaminiaiMasyvai();
+                    break;
+                case 2:
+                    WithRandomNumbersArrays(0);
+                    break;
+                case 3: 
+                    WithRandomNumbersArrays(1);
+                    break;
+                case 4:
+                cout << "Darbas baigiamas" << endl;
+
+                break;
+                default:
+                    cout << "Blogas Pasirinkimas" << endl;
+            }
+
+
+
     }
-
     return 0;
 }
