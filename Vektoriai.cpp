5869a2f45ab248c91bd691ff6a95e5551529f388
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-03-01 13:16:36 +0200

    fixes

--- Vektoriai.cpp
+++ Vektoriai.cpp
@@ -59,9 +59,7 @@ void WithVectors() {
             cin >> choice;
     
         } while (choice == "taip");
-    
-        cout << "Vardas              Pavarde             Galutinis (Vid.)/ Galutinis (Med.)" << endl;
-        cout << "--------------------------------------------------------------------------" << endl;
+
     
         for (const auto& studentas : studentai) {
             double Vidurkis = 0, Mediana = 0, GalutinisBalasVidurkis = 0, GalutinisBalasMediana = 0;
@@ -86,10 +84,134 @@ void WithVectors() {
             GalutinisBalasMediana = 0.4 * Mediana + 0.6 * studentas.egzaminoPazimys;
             GalutinisBalasVidurkis = 0.4 * Vidurkis + 0.6 * studentas.egzaminoPazimys;
     
-            cout << left << setw(20) << studentas.vardas  
-                      << left << setw(20) << studentas.pavarde  
-                      << left << setw(18) << fixed << setprecision(2) << GalutinisBalasVidurkis 
-                      << left << setw(18) << fixed << setprecision(2) << GalutinisBalasMediana << endl;
+        }
+
+        
+if (RusiavimoPasirinkimas == 1){ //sortint pagal varda/
+    sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+        return a.vardas < b.vardas;
+    });
+    
+}
+
+
+if (RusiavimoPasirinkimas == 2){ //sortint pagal pavarde
+    sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+        return a.pavarde < b.pavarde;
+    });
+    
+}
+
+if (RusiavimoPasirinkimas == 3){ //sortint pagal Vidurki
+
+        sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+            double avgA = 0, avgB = 0;
+            for (int pazymys : a.pazymiai) {
+                avgA += pazymys;
+            }
+            if (!a.pazymiai.empty()) avgA /= a.pazymiai.size();
+            for (int pazymys : b.pazymiai) {
+                avgB += pazymys;
+            }
+            if (!b.pazymiai.empty()) avgB /= b.pazymiai.size();
+    
+            double galutinisA = 0.4 * avgA + 0.6 * a.egzaminoPazimys;
+            double galutinisB = 0.4 * avgB + 0.6 * b.egzaminoPazimys;
+            return galutinisA > galutinisB;
+        });
+    };
+
+    if (RusiavimoPasirinkimas == 4){ //Sortint pagal mediana
+        sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+            vector<int> sortedA = a.pazymiai;
+            sort(sortedA.begin(), sortedA.end());
+            double medianA = 0;
+            
+            
+                int pazymiuKiekis = sortedA.size();
+                if (pazymiuKiekis % 2 == 0) {
+                    medianA = (sortedA[pazymiuKiekis / 2 - 1] + sortedA[pazymiuKiekis / 2]) / 2.0;
+                } else {
+                    medianA = sortedA[pazymiuKiekis / 2];
+                }
+            
+    
+            vector<int> sortedB = b.pazymiai;
+            sort(sortedB.begin(), sortedB.end());
+            double medianB = 0;
+            
+                int n = sortedB.size();
+                if (n % 2 == 0) {
+                    medianB = (sortedB[n / 2 - 1] + sortedB[n / 2]) / 2.0;
+                } else {
+                    medianB = sortedB[n / 2];
+                }
+        
+                double galutinisA = 0.4 * medianA + 0.6 * a.egzaminoPazimys;
+                double galutinisB = 0.4 * medianB + 0.6 * b.egzaminoPazimys;
+                return galutinisA > galutinisB;
+        });
+    }
+
+        if (IsvedimoPasirinkimas == 1){
+
+        cout << "Vardas              Pavarde             Galutinis (Vid.)/ Galutinis (Med.)" << endl;
+        cout << "--------------------------------------------------------------------------" << endl;
+        }
+        
+if (IsvedimoPasirinkimas == 2){
+
+    std::ofstream OutputFile("StudentaiOutput.txt", std::ios::trunc); 
+    OutputFile << "Vardas              Pavarde             Galutinis (Vid.)/ Galutinis (Med.)\n";
+    OutputFile << "--------------------------------------------------------------------------\n";
+    
+
+}
+
+        for (const auto& studentas : studentai) {
+            double Vidurkis = 0, Mediana = 0, GalutinisBalasVidurkis = 0, GalutinisBalasMediana = 0;
+
+            for (int pazymys : studentas.pazymiai) {
+                Vidurkis += pazymys;
+            }
+
+            if (!studentas.pazymiai.empty()) {
+                Vidurkis /= studentas.pazymiai.size();
+            }
+
+    
+            vector<int> sortedPazymiai = studentas.pazymiai;
+            sort(sortedPazymiai.begin(), sortedPazymiai.end());
+
+            if (!sortedPazymiai.empty()) {
+                int n = sortedPazymiai.size();
+                if (n % 2 == 0) {
+                    Mediana = (sortedPazymiai[n / 2 - 1] + sortedPazymiai[n / 2]) / 2.0;
+                } else {
+                    Mediana = sortedPazymiai[n / 2];
+                }
+            }
+
+        
+            GalutinisBalasMediana = 0.4 * Mediana + 0.6 * studentas.egzaminoPazimys;
+            GalutinisBalasVidurkis = 0.4 * Vidurkis + 0.6 * studentas.egzaminoPazimys;
+
+            if (IsvedimoPasirinkimas == 1){
+              output << left << setw(20) << studentas.vardas
+            << left << setw(20) << studentas.pavarde
+            << left << setw(18) << fixed << setprecision(2) << GalutinisBalasVidurkis
+            << left << setw(18) << fixed << setprecision(2) << GalutinisBalasMediana
+            << '\n';
+
+            }
+            if (IsvedimoPasirinkimas == 2){
+                std::ofstream OutputFile("StudentaiOutput.txt", std::ios::trunc); 
+                OutputFile << left << setw(20) << studentas.vardas
+                << left << setw(20) << studentas.pavarde
+                << left << setw(18) << fixed << setprecision(2) << GalutinisBalasVidurkis
+                << left << setw(18) << fixed << setprecision(2) << GalutinisBalasMediana
+                << '\n';
+            }
         }
     }
     
