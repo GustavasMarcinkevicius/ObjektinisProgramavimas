5ae7f5836d816eacb08427c111e0ca6d5f850313
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-03-20 20:09:26 +0200

    Lists 2nd strategy

--- SuListais.cpp
+++ SuListais.cpp
@@ -1,6 +1,8 @@
 #include "Includes.h"
 
 void SuListais(int StrategijosPasirinkimas){
+    std::chrono::high_resolution_clock::time_point KietekuPabaiga;
+    std::chrono::duration<double> KietekuLaikas;
     int StudentuKiekis;
     int Rusiavimas;
     std::ostringstream output;
@@ -88,7 +90,7 @@ void SuListais(int StrategijosPasirinkimas){
     
     std::chrono::high_resolution_clock::time_point RusiavimoPradzia = std::chrono::high_resolution_clock::now();
 
-    if (StrategijosPasirinkimas == 1){
+if(StrategijosPasirinkimas == 1){
 
     for  (auto& studentas : studentai) {
         if (Rusiavimas == 3) { 
@@ -105,14 +107,59 @@ void SuListais(int StrategijosPasirinkimas){
             }
         }
     }
-};
-      
+
+}
+
+// JEIGU MANO PARINKA STRATEGIJA NEATITINKA 2 STRATEGIJOS REIKALAVIMU IR REIKIA NAUDOTI ERASE, O NE POP_BACK
+// else if (StrategijosPasirinkimas == 2){
+//         if (Rusiavimas == 3) {
+//             for (auto it = studentai.begin(); it != studentai.end(); ) {
+//             if (it->GalutinisBalasVidurkis < 5) {
+//                 vargsiukai.push_back(*it);
+//                 it = studentai.erase(it);  
+//             } else {
+//                 ++it;
+//             };
+//             }
+//         }
+//         if (Rusiavimas == 4) {
+//             for (auto it = studentai.begin(); it != studentai.end(); ) {
+//             if (it->GalutinisBalasMediana < 5) {
+//                 vargsiukai.push_back(*it);
+//                 it = studentai.erase(it);  
+//             } else {
+//                 ++it;
+//             };
+//             }
+//         }
+
+else if (StrategijosPasirinkimas == 2) {
+
+    if (Rusiavimas == 3) {
+    
+        while (!studentai.empty() && studentai.back().GalutinisBalasVidurkis < 5) {
+            vargsiukai.push_back(studentai.back());
+            studentai.pop_back(); 
+        }
+    }
+
+    if (Rusiavimas == 4) {
+        while (!studentai.empty() && studentai.back().GalutinisBalasMediana < 5) {
+            vargsiukai.push_back(studentai.back()); 
+            studentai.pop_back();  
+        }
+    }
+
+}
+
     std::chrono::high_resolution_clock::time_point RusiavimoPabaiga = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> RusiavimoLaikas = RusiavimoPabaiga - RusiavimoPradzia;
 
     std::chrono::high_resolution_clock::time_point KietekuPradzia = std::chrono::high_resolution_clock::now();
     OutputFile1 << "Vardas              Pavarde             Galutinis (Vid.)/ Galutinis (Med.)\n";
     OutputFile1 << "--------------------------------------------------------------------------\n";
+
+if (StrategijosPasirinkimas == 1){
 
     for (const auto& studentas : kietekai) {
         OutputFile1 << left << setw(20) << studentas.vardas
@@ -121,8 +168,9 @@ void SuListais(int StrategijosPasirinkimas){
                    << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasMediana
                    << '\n';
     }
-    std::chrono::high_resolution_clock::time_point KietekuPabaiga = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> KietekuLaikas = KietekuPabaiga - KietekuPradzia;
+    
+  KietekuPabaiga = std::chrono::high_resolution_clock::now();
+  KietekuLaikas = KietekuPabaiga - KietekuPradzia;
 
     
     std::chrono::high_resolution_clock::time_point VargsiukuPradzia = std::chrono::high_resolution_clock::now();
@@ -135,9 +183,36 @@ void SuListais(int StrategijosPasirinkimas){
                    << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasMediana
                    << '\n';
     }
+}
+
+if (StrategijosPasirinkimas == 2){
+
+    for (const auto& studentas : studentai) {
+        OutputFile1 << left << setw(20) << studentas.vardas
+                   << left << setw(20) << studentas.pavarde
+                   << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasVidurkis
+                   << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasMediana
+                   << '\n';
+    }
+    
+  KietekuPabaiga = std::chrono::high_resolution_clock::now();
+  KietekuLaikas = KietekuPabaiga - KietekuPradzia;
+
+    
+    std::chrono::high_resolution_clock::time_point VargsiukuPradzia = std::chrono::high_resolution_clock::now();
+    OutputFile2 << "Vardas              Pavarde             Galutinis (Vid.)/ Galutinis (Med.)\n";
+    OutputFile2 << "--------------------------------------------------------------------------\n";
+    for (const auto& studentas : vargsiukai) {
+        OutputFile2 << left << setw(20) << studentas.vardas
+                   << left << setw(20) << studentas.pavarde
+                   << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasVidurkis
+                   << left << setw(18) << fixed << setprecision(2) << studentas.GalutinisBalasMediana
+                   << '\n';
+    }
+}
+
     std::chrono::high_resolution_clock::time_point VargsiukuPabaiga = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> VargsiukuLaikas = KietekuPabaiga - KietekuPradzia;
-    
     std::chrono::high_resolution_clock::time_point ProgramEnd = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> ProgramDuration = ProgramEnd - Readingstart;
     cout << "Failo su " << StudentuKiekis << " studentu nuskaitymo laikas: " << Readingduration.count() << " sekundes" << endl;
@@ -146,7 +221,4 @@ void SuListais(int StrategijosPasirinkimas){
     cout << StudentuKiekis << " Studentu kieteku irasymo laikas: " << KietekuLaikas.count() << " sekundes" << endl;
     cout << StudentuKiekis << " Studentu vargsiuku irasymo laikas: " << VargsiukuLaikas.count() << " sekundes" << endl;
     cout << "Visos programos veikimo laikas: " << ProgramDuration.count() << " sekundes" << endl;
-
-  
-
-    };
+}
