03a6b19a9b467d174dc6782fad1789db406c779e
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-03-21 14:06:40 +0200

    bug fixes

--- SuDeque.cpp
+++ SuDeque.cpp
@@ -114,47 +114,48 @@ if(StrategijosPasirinkimas == 1){
 
 }
 
-// JEIGU MANO PARINKA STRATEGIJA NEATITINKA 2 STRATEGIJOS REIKALAVIMU IR REIKIA NAUDOTI ERASE, O NE POP_BACK
-// else if (StrategijosPasirinkimas == 2){
-//         if (Rusiavimas == 3) {
-//             for (auto it = studentai.begin(); it != studentai.end(); ) {
-//             if (it->GalutinisBalasVidurkis < 5) {
-//                 vargsiukai.push_back(*it);
-//                 it = studentai.erase(it);  
-//             } else {
-//                 ++it;
-//             };
-//             }
-//         }
-//         if (Rusiavimas == 4) {
-//             for (auto it = studentai.begin(); it != studentai.end(); ) {
-//             if (it->GalutinisBalasMediana < 5) {
-//                 vargsiukai.push_back(*it);
-//                 it = studentai.erase(it);  
-//             } else {
-//                 ++it;
-//             };
-//             }
-//         }
 
-else if (StrategijosPasirinkimas == 2) {
+else if (StrategijosPasirinkimas == 2){
+        if (Rusiavimas == 3) {
+            for (auto it = studentai.begin(); it != studentai.end(); ) {
+            if (it->GalutinisBalasVidurkis < 5) {
+                vargsiukai.push_back(*it);
+                it = studentai.erase(it);  
+            } else {
+                ++it;
+            };
+            }
+        }
+        if (Rusiavimas == 4) {
+            for (auto it = studentai.begin(); it != studentai.end(); ) {
+            if (it->GalutinisBalasMediana < 5) {
+                vargsiukai.push_back(*it);
+                it = studentai.erase(it);  
+            } else {
+                ++it;
+            };
+            }
+        }
+    }
 
-    if (Rusiavimas == 3) {
+// else if (StrategijosPasirinkimas == 2) {
+
+//     if (Rusiavimas == 3) {
     
-        while (!studentai.empty() && studentai.back().GalutinisBalasVidurkis < 5) {
-            vargsiukai.push_back(studentai.back());
-            studentai.pop_back(); 
-        }
-    }
+//         while (!studentai.empty() && studentai.back().GalutinisBalasVidurkis < 5) {
+//             vargsiukai.push_back(studentai.back());
+//             studentai.pop_back(); 
+//         }
+//     }
 
-    if (Rusiavimas == 4) {
-        while (!studentai.empty() && studentai.back().GalutinisBalasMediana < 5) {
-            vargsiukai.push_back(studentai.back()); 
-            studentai.pop_back();  
-        }
-    }
+//     if (Rusiavimas == 4) {
+//         while (!studentai.empty() && studentai.back().GalutinisBalasMediana < 5) {
+//             vargsiukai.push_back(studentai.back()); 
+//             studentai.pop_back();  
+//         }
+//     }
 
-}
+// }
 
     std::chrono::high_resolution_clock::time_point RusiavimoPabaiga = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> RusiavimoLaikas = RusiavimoPabaiga - RusiavimoPradzia;
